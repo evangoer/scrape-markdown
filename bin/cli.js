59337670aa15847508fbd10b7b823c0591490ea6
Evan Goer <evan@goer.org>
2014-01-07 21:56:57 -0800

    Add explicit usage option

--- bin/cli.js
+++ bin/cli.js
@@ -2,29 +2,26 @@
 var scrape = require('../index.js').scrape,
     optimist = require('optimist'),
     argv = optimist
-            .usage('Usage: $0 -s [selector] [url]...')
+            .usage('Usage: $0 -h -s [selector] [url]...')
+            .boolean('h')
+            .alias('h', 'help')
+            .describe('h', 'Display usage')
             .alias('s', 'selector')
             .describe('s', 'Scrape HTML from each page using this selector')
             .argv;
-
 
 function readStdin() {
     var data = '';
     process.stdin.resume();
     process.stdin.setEncoding('utf8');
 
-    process.stdin.on('data', function (chunk) {
-        data += chunk;
-    });
+    process.stdin.on('data', function (chunk) { data += chunk; });
+    process.stdin.on('end', function () { scrape(argv, data); });
+}
 
-    process.stdin.on('end', function () {
-        if (data === '') {
-            console.log('Please supply some URLs or pipe HTML to stdin.\n');
-            optimist.showHelp();
-            process.exit(1);
-        }
-        scrape(argv, data);
-    });
+if (argv.help) {
+    optimist.showHelp();
+    process.exit();
 }
 
 if (argv._.length === 0) {
